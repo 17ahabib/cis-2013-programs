19cabbc43b663f56652cb9d65ea524a79fa5c78a
17ahabib <58060191+17ahabib@users.noreply.github.com>
2020-02-03 11:47:43 -0800

    Add files via upload

---
+++ age.js
@@ -0,0 +1,11 @@
+var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
+floatAge = parseFloat(prompt("Enter age"));
+floatDays = parseFloat(floatAge * 365.25);
+intWeeks = parseInt(floatDays / 7);
+floatMonths = parseFloat(floatAge * 12);
+intFortnights = parseInt(floatDays / 14);
+alert("Your age is: " + floatAge);
+alert("Your age in days is: " + floatDays);
+alert("Your age in weeks is: " + intWeeks);
+alert("Your age in months is: " + floatMonths);
+alert("Your age in fortnights is: " + intFortnights);
